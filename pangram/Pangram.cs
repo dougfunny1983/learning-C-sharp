a70cb7f54c9e3fce8317eab054dcf7c210c176a8
douglas eduardo <douglaseduardo220483@gmail.com>
2020-10-02 18:01:09 -0300

    Pangrama
    Pangrama, ou pantograma, é uma frase em que são usadas todas as letras do alfabeto de determinada língua

---
+++ pangram/Pangram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public static class Pangram
+{
+    private const string Pattern = "abcdefghijklmnopqrstuvwxyz";
+
+    private const string PatternRepalce = @"[^\\s0-9a-zA-Z]+";
+
+    public static bool IsPangram(string input)
+    {
+
+        string new_input = Regex.Replace(input, PatternRepalce, "").ToLower();
+
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return false;
+        }
+
+        foreach (var x in Pattern)
+        {
+            var isTrue = new_input.Contains(x);
+            if (!isTrue)
+            {
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
