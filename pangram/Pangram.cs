696626a55934b2af1ff2fe1d9fac1467c82b337b
douglas eduardo <douglaseduardo220483@gmail.com>
2020-10-02 21:02:31 -0300

    isogram
    Esse é o isograma. Basicamente não se pode repetir os caracteres na frase. Usei o conceito de hashSet aprendido em python, na trybe no bloco CS

--- pangram/Pangram.cs
+++ pangram/Pangram.cs
@@ -1,31 +1,8 @@
-﻿using System;
-using System.Text.RegularExpressions;
+﻿using System.Linq;
 
 public static class Pangram
 {
     private const string Pattern = "abcdefghijklmnopqrstuvwxyz";
 
-    private const string PatternRepalce = @"[^\\s0-9a-zA-Z]+";
-
-    public static bool IsPangram(string input)
-    {
-
-        string new_input = Regex.Replace(input, PatternRepalce, "").ToLower();
-
-        if (string.IsNullOrWhiteSpace(input))
-        {
-            return false;
-        }
-
-        foreach (var x in Pattern)
-        {
-            var isTrue = new_input.Contains(x);
-            if (!isTrue)
-            {
-                return false;
-            }
-        }
-        return true;
-
-    }
+    public static bool IsPangram(string input) => Pattern.All(input.ToLower().Contains);
 }
