6e8afc9c56dc4e7e71eda3921d1e3809639dd06e
douglas eduardo <douglaseduardo220483@gmail.com>
2020-10-02 21:39:48 -0300

    Acronym
    Esse desafio é relacionado a colocar em upercase as primeiras letras de uma string. Acontece que há caracteres especias inseridos. Excelente desafio.

---
+++ acronym/Acronym.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public static class Acronym
+{
+    private const string Pattern = @"[^\\s0-9a-zA-Z\']+";
+    public static string Abbreviate(string phrase)
+    {
+        var array = Regex.Split(phrase, Pattern);
+        var str = "";
+        foreach (var x in array) 
+        {
+            str += x[0];
+        }
+
+        return str.ToUpper();
+    }
+}
