f0311caf3072bbb36dd6ee234e7550dac4a60388
douglas eduardo <douglaseduardo220483@gmail.com>
2020-10-07 10:29:58 -0300

    collatz-conjecture
    A conjectura de Collatz ou problema 3x + 1 pode ser resumido da seguinte forma:
    
    Pegue qualquer número inteiro positivo n. Se n for par, divida n por 2 para obter n / 2. Se n for ímpar, multiplique n por 3 e adicione 1 para obter 3n + 1. Repita o processo indefinidamente. A conjectura afirma que não importa com qual número você comece, você sempre alcançará 1 eventualmente.
    
    Dado um número n, retorna o número de etapas necessárias para chegar a 1.

---
+++ collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        if (number <= 0) throw new ArgumentOutOfRangeException("The expected value must be a natural number");
+
+        int n = number;
+        int cont = 0;
+        while (n > 1)
+        {
+            if (n % 2 == 0)
+            {
+                n /= 2;
+            }
+            else 
+            {
+                n = (n * 3) + 1;
+            }
+            cont++;
+        }
+        return cont;
+    }
+}
