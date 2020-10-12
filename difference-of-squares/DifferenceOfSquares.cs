ce5f4e598a2e21a4a20d262bc6f380484cb68a72
douglas eduardo <douglaseduardo220483@gmail.com>
2020-10-11 22:43:25 -0300

    Diferença de quadrados
    Encontre a diferença entre o quadrado da soma e a soma dos quadrados dos primeiros N números naturais.
    
    O quadrado da soma dos dez primeiros números naturais é (1 + 2 + ... + 10) ² = 55² = 3025.
    
    A soma dos quadrados dos primeiros dez números naturais é 1² + 2² + ... + 10² = 385.
    
    Portanto, a diferença entre o quadrado da soma dos dez primeiros números naturais e a soma dos quadrados dos dez primeiros números naturais é 3025 - 385 = 2640.
    
    Não se espera que você mesmo descubra uma solução eficiente para isso a partir dos primeiros princípios; a pesquisa é permitida, na verdade, incentivada. Encontrar o melhor algoritmo para o problema é uma habilidade chave na engenharia de software.

---
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public static class DifferenceOfSquares
+
+{
+    private static int Squares(int number) => (int)Math.Pow(number, 2);
+    // não deu para fazer com 1UL << n! Simplismente não chego ao resultado esperado;
+
+    public static int CalculateSquareOfSum(int max) => Squares(Enumerable.Range(1, max).Sum());
+
+    public static int CalculateSumOfSquares(int max) => Enumerable.Range(1, max).Select(x => Squares(x)).Sum();
+
+    public static int CalculateDifferenceOfSquares(int max) => CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+ 
+}
