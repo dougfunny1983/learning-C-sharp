b910ccaf9d7f873fc22c13e725d6eb4db2d0a6f3
douglas eduardo <douglaseduardo220483@gmail.com>
2020-10-15 13:43:02 -0300

    Triângulo de Pascal
    Calcule o triângulo de Pascal até um determinado número de linhas.
    
    No Triângulo de Pascal, cada número é calculado somando-se os números à direita e à esquerda da posição atual na linha anterior.

---
+++ pascals-triangle/PascalsTriangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class PascalsTriangle
+{
+    public static IEnumerable<IEnumerable<int>> Calculate(int rows)
+    {
+
+        int[][] answer = new int[rows][];
+
+        if (rows == 0) return answer;
+
+        for (int row = 0; row < rows; row++)
+        {
+            answer[row] = new int[row + 1];
+        }
+
+        answer[0][0] = 1;
+
+        for (int row = 0; row < rows - 1; row++)
+        {
+            for (int col = 0; col <= row; col++)
+            {
+                answer[row + 1][col] += answer[row][col];
+                answer[row + 1][col + 1] += answer[row][col];
+            }
+        }
+  
+        return answer;
+
+    }
+
+}
