04cb06a970b89354b4ae5ef9ccecdb2db8ecd970
douglas eduardo <douglaseduardo220483@gmail.com>
2020-10-22 12:45:57 -0300

    Números Armstrong
    Um número Armstrong é um número que é a soma de seus próprios dígitos, cada um elevado à potência do número de dígitos.
    
    Por exemplo:
    
    9 é um número de Armstrong, porque 9 = 9^1 = 9
    10 não é um número Armstrong, porque10 != 1^2 + 0^2 = 1
    153 é um número Armstrong, porque: 153 = 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
    154 não é um número Armstrong, porque:154 != 1^3 + 5^3 + 4^3 = 1 + 125 + 64 = 190
    Escreva algum código para determinar se um número é um número Armstrong.

---
+++ armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ArmstrongNumbers
+{
+
+    private static IEnumerable<int> GetUDCM(int number) 
+    {
+        List<int> house = new List<int>();
+
+        int module = 10;
+        int division = 1;
+
+        while ((number * 10) > module)
+        {
+            house.Add(number % module / division);
+            module *= 10;
+            division *= 10;
+        }
+
+        return house;
+    }
+
+    public static bool IsArmstrongNumber(int number)
+    {
+       double length = Math.Floor(Math.Log10(number) + 1);
+       
+       double answer = GetUDCM(number).Aggregate(0, (acc, x) => (int)(acc + Math.Pow(x, length)));
+
+       return answer == number;
+    }
+}
