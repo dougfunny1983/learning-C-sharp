650e7ff69376f7480b8df2e4caabdea07b30153b
douglas eduardo <douglaseduardo220483@gmail.com>
2020-10-23 14:31:17 -0300

    Números racionais
    Um número racional é definido como o quociente de dois inteiros ae b, chamado numerador e denominador, respectivamente, onde b != 0.
    
    O valor absoluto |r|do número racional r = a/bé igual a |a|/|b|.
    
    A soma de dois números racionais r1 = a1/b1e r2 = a2/b2é r1 + r2 = a1/b1 + a2/b2 = (a1 * b2 + a2 * b1) / (b1 * b2).
    
    A diferença de dois números racionais r1 = a1/b1e r2 = a2/b2é r1 - r2 = a1/b1 - a2/b2 = (a1 * b2 - a2 * b1) / (b1 * b2).
    
    O produto (multiplicação) de dois números racionais r1 = a1/b1e r2 = a2/b2é r1 * r2 = (a1 * a2) / (b1 * b2).
    
    Dividir um número racional r1 = a1/b1por outro r2 = a2/b2é r1 / r2 = (a1 * b2) / (a2 * b1)se a2 * b1não for zero.
    
    A exponenciação de um número racional r = a/bpara uma potência inteira não negativa né r^n = (a^n)/(b^n).
    
    A exponenciação de um número racional r = a/bpara uma potência inteira negativa né r^n = (b^m)/(a^m), onde m = |n|.
    
    A exponenciação de um número racional r = a/bpara um número real (ponto flutuante) xé o quociente (a^x)/(b^x), que é um número real.
    
    A exponenciação de um número real xpara um número racional r = a/bé x^(a/b) = root(x^a, b), onde root(p, q)é a qenésima raiz de p.
    
    Implemente as seguintes operações:
    
    adição, subtração, multiplicação e divisão de dois números racionais,
    valor absoluto, exponenciação de um dado número racional para uma potência inteira, exponenciação de um dado número racional para uma potência real (ponto flutuante), exponenciação de um número real para um número racional.
    Sua implementação de números racionais deve sempre ser reduzida aos termos mais baixos. Por exemplo, 4/4deve reduzir a 1/1, 30/60deve reduzir a 1/2, 12/8deve reduzir a 3/2, etc. Para reduzir um número racional r = a/b, divida ae bpelo máximo divisor comum (mdc) de ae b. Assim, por exemplo, gcd(12, 8) = 4, por isso r = 12/8pode ser reduzido a (12/4)/(8/4) = 3/2.
    
    Suponha que a linguagem de programação que você está usando não tenha uma implementação de números racionais.

---
+++ rational-numbers/RationalNumbers.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+
+public static class RealNumberExtension
+{
+    public static double Expreal(this int realNumber, RationalNumber r)
+    {
+        throw new NotImplementedException("You need to implement this extension method.");
+    }
+}
+
+public struct RationalNumber
+{
+    public int Numerator { get; private set; }
+    public int Denominator { get; private set; }
+
+    //private readonly int Numerator;
+    //private readonly int Denominator;
+
+    public RationalNumber(int numerator, int denominator)
+    {
+        Numerator = numerator;
+        Denominator = denominator;
+    }
+
+    //public static Source operator +(Source s1, Source s2) // SOMA
+    //{
+    //    return new Source { Number = s1.Number + s2.Number };
+    //}
+
+    public static RationalNumber operator +(RationalNumber r1, RationalNumber r2)
+    {
+        //return new RationalNumber ( r1.Numerator + r2.Denominator );
+        return new RationalNumber ( r1.Numerator + r2.Denominator, r1.Numerator + r2.Denominator );
+        //return new RationalNumber { r1.Numerator + r2.Denominator };
+    }
+
+    public static RationalNumber operator -(RationalNumber r1, RationalNumber r2)
+    {
+        throw new NotImplementedException("You need to implement this operator.");
+    }
+
+    public static RationalNumber operator *(RationalNumber r1, RationalNumber r2)
+    {
+        throw new NotImplementedException("You need to implement this operator.");
+    }
+
+    public static RationalNumber operator /(RationalNumber r1, RationalNumber r2)
+    {
+        throw new NotImplementedException("You need to implement this operator.");
+    }
+
+    public RationalNumber Abs()
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public RationalNumber Reduce()
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public RationalNumber Exprational(int power)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public double Expreal(int baseNumber)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
