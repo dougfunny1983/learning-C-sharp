b3095b627e5b3786d030b7fc81d6ca79feb4bcb0
douglas eduardo <douglaseduardo220483@gmail.com>
2020-10-27 14:31:08 -0300

    Dardos
    Escreva uma função que retorne os pontos ganhos em um único lançamento de um jogo de dardos.
    
    Dardos é um jogo em que os jogadores lançam dardos a um alvo .
    
    Em nossa instância particular do jogo, o alvo recompensa com 4 quantidades diferentes de pontos, dependendo de onde o dardo cair:
    
    Se o dardo cair fora do alvo, o jogador não ganha pontos (0 pontos).
    Se o dardo cair no círculo externo do alvo, o jogador ganha 1 ponto.
    Se o dardo cair no círculo central do alvo, o jogador ganha 5 pontos.
    Se o dardo cair no círculo interno do alvo, o jogador ganha 10 pontos.
    O círculo externo tem um raio de 10 unidades (isso é equivalente ao raio total de todo o alvo), o círculo do meio tem um raio de 5 unidades e o círculo interno tem um raio de 1. Claro, todos eles estão centrados no mesmo ponto (ou seja, os círculos são concêntricos ) definido pelas coordenadas (0, 0).
    
    Escreva uma função que, dado um ponto no alvo (definido por suas realcoordenadas cartesianas xe y), retorne a quantia correta ganha por um dardo pousando naquele ponto.

---
+++ darts/Darts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class Darts
+{
+
+    private static int Distance(double x, double y) 
+    {
+        double distance = x * x + y * y;
+ 
+        return (x, y) switch
+        {
+            var (_, _) when distance > Math.Pow(10.0F, 2) => 0,
+            var (_, _) when distance > Math.Pow(5.0F, 2)   => 1,
+            var (_, _) when distance > Math.Pow(1.0F, 2)  => 5,
+            _ => 10,
+        };
+    } 
+
+    public static int Score(double x, double y) => Distance(x, y);
+}
