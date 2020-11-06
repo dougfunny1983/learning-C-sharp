a1e36ecb4a35d4e15f398141fb63400d3c1bc93e
douglas eduardo <douglaseduardo220483@gmail.com>
2020-11-06 11:50:58 -0300

    Freqüência de letra paralela
    Conte a frequência das letras em textos usando computação paralela.
    
    Paralelismo significa fazer coisas em paralelo que também podem ser feitas sequencialmente. Um exemplo comum é a contagem da frequência das letras. Crie uma função que retorne a frequência total de cada letra em uma lista de textos e que empregue paralelismo.

---
+++ parallel-letter-frequency/ParallelLetterFrequency.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class ParallelLetterFrequency
+{
+    public static Dictionary<char, int> Calculate(IEnumerable<string> texts) => texts
+            .AsParallel()
+            .SelectMany(text => text.ToLower())
+            .Where(cha => char.IsLetter(cha))
+            .GroupBy(text => text)
+            .ToDictionary(text => text.Key, text => text.Count());
+}
