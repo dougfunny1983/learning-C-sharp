39347c52d1cebc0c520ca0479097d9f8478b4a3d
douglas eduardo <douglaseduardo220483@gmail.com>
2020-11-17 10:27:36 -0300

    Pesquisa Binária
    Implemente um algoritmo de pesquisa binária.
    
    Pesquisar uma coleção classificada é uma tarefa comum. Um dicionário é uma lista classificada de definições de palavras. Dada uma palavra, pode-se encontrar sua definição. Uma lista telefônica é uma lista ordenada de nomes, endereços e números de telefone de pessoas. Saber o nome de alguém permite encontrar rapidamente seu número de telefone e endereço.
    
    Se a lista a ser pesquisada contiver mais do que alguns itens (uma dúzia, digamos), uma pesquisa binária exigirá muito menos comparações do que uma pesquisa linear, mas impõe o requisito de que a lista seja classificada.
    
    Na ciência da computação, uma pesquisa binária ou algoritmo de pesquisa de meio intervalo encontra a posição de um valor de entrada especificado (a "chave" de pesquisa) em uma matriz classificada por valor de chave.
    
    Em cada etapa, o algoritmo compara o valor da chave de pesquisa com o valor da chave do elemento do meio da matriz.
    
    Se as chaves corresponderem, um elemento correspondente foi encontrado e seu índice, ou posição, é retornado.
    
    Caso contrário, se a chave de pesquisa for menor que a chave do elemento do meio, o algoritmo repete sua ação na submatriz à esquerda do elemento do meio ou, se a chave de pesquisa for maior, na submatriz à direita.
    
    Se a matriz restante a ser pesquisada estiver vazia, a chave não pode ser encontrada na matriz e uma indicação especial "não encontrado" é retornada.
    
    Uma pesquisa binária divide pela metade o número de itens a serem verificados a cada iteração, portanto, localizar um item (ou determinar sua ausência) leva um tempo logarítmico. Uma pesquisa binária é um algoritmo de pesquisa dicotômica para dividir e conquistar.

---
+++ binary-search/BinarySearch.cs
@@ -0,0 +1,20 @@
+﻿public static class BinarySearch
+{
+    private static int SearchBinary(int value, int[] array, int low, int high)
+    {
+        if (low > high) return -1;
+        int mid = low + (high - low) / 2;
+        int midValue = array[mid];
+
+        if (midValue == value)
+            return mid;
+
+        else if (midValue < value)
+            return SearchBinary(value, array, mid + 1, high);
+        else
+            return SearchBinary(value, array, low, mid - 1);
+    }
+
+    public static int Find(int[] input, int value) => input.Length < 1 ? -1 : SearchBinary(value, input, 0, input.Length - 1);
+
+}
