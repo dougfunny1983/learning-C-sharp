f327bfb89cb77f09f0bce20ff075d70b247d93bb
douglas eduardo <douglaseduardo220483@gmail.com>
2020-11-18 18:25:04 -0300

    Campo Minado
    Adicione os números a um tabuleiro de caça-minas.
    
    O Campo Minado é um jogo popular em que o usuário deve encontrar as minas usando dicas numéricas que indicam quantas minas estão diretamente adjacentes (horizontalmente, verticalmente, diagonalmente) a um quadrado.
    
    Neste exercício, você deve criar algum código que conte o número de minas adjacentes a um quadrado e transforme as placas como este (onde * indica uma mina):

---
+++ minesweeper/Minesweeper.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class Minesweeper
+{
+    public static string[] Annotate(string[] input)
+    {
+        char[][] answer = input.Select(t => t.ToCharArray()).ToArray();
+        int rows = answer.Length;
+        int cols = (rows > 0) ? answer[0].Length : 0;
+
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+            {
+                if (answer[i][j] == '*')
+                {
+                    IEnumerable<(int row, int cols)> pumpPosition = PumpPosition(i, j)
+                        .Where(t => t != (i, j))
+                        .Where(t => t.row >= 0 && t.col >= 0)
+                        .Where(t => t.row < rows && t.col < cols);
+
+                    foreach ((int col, int line) in pumpPosition)
+                    {
+                        switch (answer[col][line])
+                        {
+                            case ' ':
+                                answer[col][line] = '1';
+                                break;
+                            case char n when (n >= '1' && n < '8'):
+                                answer[col][line]++;
+                                break;
+                        }
+                    }
+                }
+            }
+
+        return answer.Select(t => new string(t)).ToArray();
+    }
+
+
+    private static IEnumerable<(int row, int col)> PumpPosition(int row, int col)
+    {
+        for (int i = row - 1; i <= row + 1; i++)
+            for (int j = col - 1; j <= col + 1; j++)
+            {
+                yield return (i, j);
+            }
+    }
+}
