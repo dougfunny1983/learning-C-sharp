04555a3f14ead77f2bda2e6eb54068f77a664463
douglas eduardo <douglaseduardo220483@gmail.com>
2020-11-18 17:33:34 -0300

    Matriz
    Dada uma string que representa uma matriz de números, retorne as linhas e colunas dessa matriz.
    
    Então, dada uma string com novas linhas incorporadas como:
    
    9 8 7
    5 3 2
    6 6 7
    representando esta matriz:
    
        1  2  3
      |---------
    1 | 9  8  7
    2 | 5  3  2
    3 | 6  6  7
    seu código deve ser capaz de cuspir:
    
    Uma lista das linhas, lendo cada linha da esquerda para a direita enquanto move de cima para baixo nas linhas,
    Uma lista das colunas, lendo cada coluna de cima para baixo enquanto se move da esquerda para a direita.
    As linhas para nossa matriz de exemplo:
    
    9, 8, 7
    5, 3, 2
    6, 6, 7
    E suas colunas:
    
    9, 5, 6
    8, 3, 6
    7, 2, 7

---
+++ matrix/Matrix.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Matrix
+{
+    private readonly int[][] input;
+
+    public Matrix(string input)
+    {
+        string[] lines = input.Split('\n');
+
+        List<int[]> column = new List<int[]>();
+
+        foreach (string line in lines)
+            column
+                .Add(line.Split(' ')
+                .Select(cell => Convert.ToInt32(cell))
+                .ToArray());
+
+        this.input = column.ToArray();
+
+    }
+
+    public int[] Row(int row) => input[row - 1];
+
+    public int[] Column(int col) => input.Select(row => row[col - 1]).ToArray();
+
+}
