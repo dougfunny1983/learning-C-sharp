6c24c8669e02b978f08e3c8357ee05a649a6d43e
douglas eduardo <douglaseduardo220483@gmail.com>
2020-11-19 15:11:47 -0300

    String reversa
    Por exemplo: input: "cool" output: "looc"

---
+++ reverse-string/ReverseString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+public static class ReverseString
+{
+
+    private static IEnumerable<string> GraphemeClusters(this string s)
+    {
+        TextElementEnumerator enumerator = StringInfo.GetTextElementEnumerator(s);
+
+        while (enumerator.MoveNext())
+        {
+            yield return (string)enumerator.Current;
+        }
+    }
+    private static string ReverseGraphemeClusters(this string s) =>
+        string.Join("", s
+            .GraphemeClusters()
+            .Reverse()
+            .ToArray());
+
+    public static string Reverse(string input) => ReverseGraphemeClusters(input);
+}
