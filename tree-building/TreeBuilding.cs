b2f4ff2fedb368c3bea0534f20e1f5c4c148a0c9
douglas eduardo <douglaseduardo220483@gmail.com>
2020-11-19 14:57:16 -0300

    Construção de árvores
    Refatore um algoritmo de construção de árvore.
    
    Alguns fóruns da web têm um layout de árvore, então as postagens são apresentadas como uma árvore. No entanto, as postagens são normalmente armazenadas em um banco de dados como um conjunto não classificado de registros. Portanto, ao apresentar as postagens ao usuário, a estrutura em árvore deve ser reconstruída.
    
    Seu trabalho será refatorar um trecho de código funcional, mas lento e feio, que implementa a lógica de construção de árvore para registros altamente abstratos. Os registros contêm apenas um número de identificação e um número de identificação pai. O número de identificação está sempre entre 0 (inclusivo) e o comprimento da lista de registros (exclusivo). Todos os registros têm um ID pai inferior ao seu próprio ID, exceto para o registro raiz, que tem um ID pai igual ao seu próprio ID.

---
+++ tree-building/TreeBuilding.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TreeBuildingRecord
+{
+    public int ParentId { get; set; }
+    public int RecordId { get; set; }
+}
+
+public class Tree
+{
+    public int Id { get; set; }
+    public int ParentId { get; set; }
+
+    public List<Tree> Children { get; set; }
+
+    public bool IsLeaf => Children.Count == 0;
+}
+
+public static class TreeBuilder
+{
+    public static Tree BuildTree(IEnumerable<TreeBuildingRecord> records)
+    {
+        TreeBuildingRecord[] ordered = OrderId(records);
+
+        Tree[] nodes = new Tree[ordered.Length];
+        for (int i = 0; i < ordered.Length; i++)
+        {
+            TreeBuildingRecord record = ordered[i];
+            nodes[i] = new Tree { Id = record.RecordId, ParentId = record.ParentId, Children = new List<Tree>() };
+
+            if (record.RecordId == 0)
+                continue;
+
+            Tree parentNode = nodes[record.ParentId];
+            parentNode.Children.Add(nodes[i]);
+        }
+
+        return nodes[0];
+    }
+
+    private static readonly SystemException Err = new ArgumentException();
+
+    private static TreeBuildingRecord[] OrderId(IEnumerable<TreeBuildingRecord> records)
+    {
+        if (!records.Any())
+            throw Err;
+
+        TreeBuildingRecord[] ordered = new TreeBuildingRecord[records.Count()];
+
+        foreach (TreeBuildingRecord rec in records)
+        {
+            if (rec.RecordId >= ordered.Length)
+                throw Err;
+
+            if (rec.RecordId != 0 && rec.RecordId <= rec.ParentId)
+                throw Err;
+
+            if (rec.RecordId == 0 && rec.ParentId != 0)
+                throw Err;
+
+            ordered[rec.RecordId] = rec;
+        }
+
+        return ordered;
+    }
+}
